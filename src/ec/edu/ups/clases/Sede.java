6453a4aaf8f094926141c45098bb53ad2ae0255d
Darwin <Darwin@172.16.215.186>
2019-04-04 14:57:39 -0500

    Inicializacion de Proyecto

---
+++ src/ec/edu/ups/clases/Sede.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.clases;
+
+import java.util.List;
+
+/**
+ *
+ * @author Darwin
+ */
+public class Sede {
+    private  int codigo;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private List<Carrera>carreras;
+    
+}
