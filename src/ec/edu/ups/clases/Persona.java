61fe36a90a34267a74810d462d48e1dfcc57fe47
Darwin <Darwin@172.16.215.186>
2019-04-04 14:52:56 -0500

    Inicializacion de Proyecto

---
+++ src/ec/edu/ups/clases/Persona.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.clases;
+
+import java.util.List;
+
+/**
+ *
+ * @author Darwin
+ */
+public class Persona {
+    private int codigo;
+    private String nombre;
+    private String cedula;
+    private String telefono;
+    private String direccion;
+    private String correo;
+    private Sede sede;
+    
+}
